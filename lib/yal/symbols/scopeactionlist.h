535d50d94fc546cffb905fcc1e773e801fe77f42
Leander Beernaert <lbb-dev@pm.me>
2017-09-15 07:54:08 -0700

    Repo Cleanup

--- lib/yal/symbols/scopeactionlist.h
+++
@@ -1,5 +0,0 @@
-/*
- * Define the YAL_SCOPE_ACTION_LIST_FUNC(x) to use this somewhere
- */
-
-YAL_SCOPE_ACTION_LIST_FUNC(ObjectScopeAction)
--- lib/yal/types/typelist.h
+++
@@ -1,9 +0,0 @@
-/*
- * Define the YAL_TYPE_LIST_FUNC(x) to use this somewhere
- */
-YAL_TYPE_LIST_FUNC(BuiltinType)
-YAL_TYPE_LIST_FUNC(FunctionType)
-YAL_TYPE_LIST_FUNC(UndefinedType)
-YAL_TYPE_LIST_FUNC(StringType)
-YAL_TYPE_LIST_FUNC(ArrayType)
-YAL_TYPE_LIST_FUNC(AnyType)
